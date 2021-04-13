fcc518c5278eee58df5a0a8bdb5c51f479816234
Eric Harrison <eric@rubynooby.com>
2021-04-13 08:20:30 -0700

    setup connection to controller and fetch clients

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { API } from 'homebridge';
 
 import { PLATFORM_NAME } from './settings';
-import { ExampleHomebridgePlatform } from './platform';
+import { UnifiOsPlatform } from './platform';
 
 /**
  * This method registers the platform with Homebridge
  */
 export = (api: API) => {
-  api.registerPlatform(PLATFORM_NAME, ExampleHomebridgePlatform);
+  api.registerPlatform(PLATFORM_NAME, UnifiOsPlatform);
 };
+
