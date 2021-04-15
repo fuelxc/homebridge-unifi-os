43aa91e6f56d794f2d13f5c06a36c1fff8134461
Eric Harrison <eric@rubynooby.com>
2021-04-14 18:58:42 -0700

    choose between locks and switches for notifications

--- src/unifiClientDevice.ts
+++ src/unifiClientDevice.ts
@@ -33,49 +33,87 @@ export class UnifiClientDevice {
     this.accessory.getService(this.platform.Service.AccessoryInformation)!
       .setCharacteristic(this.platform.Characteristic.SerialNumber, clientDevice.mac);
 
-    // get the LightBulb service if it exists, otherwise create a new LightBulb service
-    // you can create multiple services for each accessory
-    this.service = this.accessory.getService(this.platform.Service.Switch) || this.accessory.addService(this.platform.Service.Switch);
+    if (this.platform.config.notifications) {
+      this.platform.log.debug('Build Lock');
+      const switchService = this.accessory.getService(this.platform.Service.Switch);
+      if (switchService) this.accessory.removeService(switchService);
 
-    // set the service name, this is what is displayed as the default name on the Home app
-    // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
+      this.service = this.buildLock()
+    } else {
+      this.platform.log.debug('Build Switch');
+      const lockService = this.accessory.getService(this.platform.Service.LockMechanism);
+      if (lockService) this.accessory.removeService(lockService);
+
+      this.service = this.buildSwitch();
+    }
+
     this.service.setCharacteristic(this.platform.Characteristic.Name, displayName);
-
-    // each service must implement at-minimum the "required characteristics" for the given service type
-    // see https://developers.homebridge.io/#/service/Lightbulb
-
-    // register handlers for the On/Off Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.On)
-      .onSet(this.setOn.bind(this))                // SET - bind to the `setOn` method below
-      .onGet(this.getOn.bind(this));               // GET - bind to the `getOn` method below
   }
 
-  updateClient(turnOn: CharacteristicValue): Promise<boolean> {
+  buildLock(): Service {
+    const service = this.accessory.getService(this.platform.Service.LockMechanism) ||
+      this.accessory.addService(this.platform.Service.LockMechanism);
+
+    service.getCharacteristic(this.platform.Characteristic.LockCurrentState)
+      .onGet(this.getLockState.bind(this));
+
+    service.getCharacteristic(this.platform.Characteristic.LockTargetState)
+      .onSet(this.setLockState.bind(this))
+      .onGet(this.getLockState.bind(this));
+
+    return service;
+  }
+
+  buildSwitch(): Service {
+    const service = this.accessory.getService(this.platform.Service.Switch) || this.accessory.addService(this.platform.Service.Switch);
+
+    service.getCharacteristic(this.platform.Characteristic.On)
+      .onSet(this.setSwitchOn.bind(this))
+      .onGet(this.getSwitchOn.bind(this));
+
+    return service;
+  }
+
+  updateClientState(turnOn: CharacteristicValue): Promise<boolean> {
     return new Promise((resolve, reject) => {
       if (turnOn) {
-        return this.platform.controller.unblockClient(this.platform.siteName, this.accessory.context.device.mac, (error, resp) => {
+        this.platform.controller.unblockClient(this.platform.siteName, this.accessory.context.device.mac, (error, resp) => {
           if (error) {
-            return reject(error);
+            this.platform.log.error('Error: ', error)
+            reject(error);
           }
+          this.platform.log.debug("updateClientState resp: ", resp)
           resolve(resp[0][0].blocked);
         });
       } else {
-        return this.platform.controller.blockClient(this.platform.siteName, this.accessory.context.device.mac, (error, resp) => {
+        this.platform.controller.blockClient(this.platform.siteName, this.accessory.context.device.mac, (error, resp) => {
           if (error) {
-            return reject(error);
+            this.platform.log.error('Error: ', error)
+            reject(error);
           }
+          this.platform.log.debug("updateClientState resp: ", resp)
           resolve(resp[0][0].blocked);
         });
       }
     });
   }
 
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
-   */
-  async setOn(value: CharacteristicValue) {
-    this.updateClient(value)
+  getClientState(): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      this.platform.controller.getBlockedUsers(this.platform.siteName, (error, userData) => {
+        if (error) {
+          this.platform.log.error('Error: ', error)
+          reject(error);
+        }
+        const isOn = !(userData[0].map(client => client.mac).includes(this.accessory.context.device.mac));
+
+        resolve(isOn);
+      });
+    });
+  }
+
+  async setSwitchOn(value: CharacteristicValue) {
+    this.updateClientState(value)
       .then(isOn => {
         this.states.On = isOn;
       })
@@ -84,30 +122,32 @@ export class UnifiClientDevice {
       });
   }
 
-  /**
-   * Handle the "GET" requests from HomeKit
-   * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
-   *
-   * GET requests should return as fast as possbile. A long delay here will result in
-   * HomeKit being unresponsive and a bad user experience in general.
-   *
-   * If your device takes time to respond you should update the status of your device
-   * asynchronously instead using the `updateCharacteristic` method instead.
+  async getSwitchOn(): Promise<CharacteristicValue> {
+    return this.getClientState();
+  }
 
-   * @example
-   * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
-   */
-  async getOn(): Promise<CharacteristicValue> {
-    // implement your own code to check if the device is on
+  async getLockState(): Promise<CharacteristicValue> {
     return new Promise((resolve, reject) => {
-      this.platform.controller.getBlockedUsers(this.platform.siteName, (error, userData) => {
-        if (error) {
-          return reject(error);
-        }
-        const isOn = !(userData[0].map(client => client.mac).includes(this.accessory.context.device.mac));
-
-        resolve(isOn);
-      });
+      this.getClientState()
+        .then(state => {
+          this.platform.log.debug('State: ', state)
+          resolve( state ? this.platform.Characteristic.LockCurrentState.UNSECURED :
+            this.platform.Characteristic.LockCurrentState.SECURED) 
+        })
+        .catch(error => {
+          this.platform.log.error('Error: ', error)
+          reject(error)
+        });
     });
+  }
+
+  async setLockState(value: CharacteristicValue) {
+    this.updateClientState(value === this.platform.Characteristic.LockCurrentState.UNSECURED)
+      .then(isOn => {
+        this.states.On = isOn;
+      })
+      .catch(error => {
+        this.platform.log.error(`Error in setOn: ${error}`);
+      });
   }
 }
