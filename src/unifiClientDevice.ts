f48b4fc23578f452705f278b19e13fcaf71b2fc9
Eric Harrison <eric@rubynooby.com>
2021-04-16 09:15:40 -0700

    readme, config and lint

--- src/unifiClientDevice.ts
+++ src/unifiClientDevice.ts
@@ -36,13 +36,17 @@ export class UnifiClientDevice {
     if (this.platform.config.notifications) {
       this.platform.log.debug('Build Lock');
       const switchService = this.accessory.getService(this.platform.Service.Switch);
-      if (switchService) this.accessory.removeService(switchService);
+      if (switchService) {
+        this.accessory.removeService(switchService);
+      }
 
-      this.service = this.buildLock()
+      this.service = this.buildLock();
     } else {
       this.platform.log.debug('Build Switch');
       const lockService = this.accessory.getService(this.platform.Service.LockMechanism);
-      if (lockService) this.accessory.removeService(lockService);
+      if (lockService) {
+        this.accessory.removeService(lockService);
+      }
 
       this.service = this.buildSwitch();
     }
@@ -79,19 +83,19 @@ export class UnifiClientDevice {
       if (turnOn) {
         this.platform.controller.unblockClient(this.platform.siteName, this.accessory.context.device.mac, (error, resp) => {
           if (error) {
-            this.platform.log.error('Error: ', error)
+            this.platform.log.error('Error: ', error);
             reject(error);
           }
-          this.platform.log.debug("updateClientState resp: ", resp)
+          this.platform.log.debug('updateClientState resp: ', resp);
           resolve(resp[0][0].blocked);
         });
       } else {
         this.platform.controller.blockClient(this.platform.siteName, this.accessory.context.device.mac, (error, resp) => {
           if (error) {
-            this.platform.log.error('Error: ', error)
+            this.platform.log.error('Error: ', error);
             reject(error);
           }
-          this.platform.log.debug("updateClientState resp: ", resp)
+          this.platform.log.debug('updateClientState resp: ', resp);
           resolve(resp[0][0].blocked);
         });
       }
@@ -102,7 +106,7 @@ export class UnifiClientDevice {
     return new Promise((resolve, reject) => {
       this.platform.controller.getBlockedUsers(this.platform.siteName, (error, userData) => {
         if (error) {
-          this.platform.log.error('Error: ', error)
+          this.platform.log.error('Error: ', error);
           reject(error);
         }
         const isOn = !(userData[0].map(client => client.mac).includes(this.accessory.context.device.mac));
@@ -130,13 +134,13 @@ export class UnifiClientDevice {
     return new Promise((resolve, reject) => {
       this.getClientState()
         .then(state => {
-          this.platform.log.debug('State: ', state)
+          this.platform.log.debug('State: ', state);
           resolve( state ? this.platform.Characteristic.LockCurrentState.UNSECURED :
-            this.platform.Characteristic.LockCurrentState.SECURED) 
+            this.platform.Characteristic.LockCurrentState.SECURED);
         })
         .catch(error => {
-          this.platform.log.error('Error: ', error)
-          reject(error)
+          this.platform.log.error('Error: ', error);
+          reject(error);
         });
     });
   }
